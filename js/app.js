8c5c5e5fc03028174876c3eb68a8ac1760e5c0b4
Maryna <textconstruction@protonmail.com>
2021-01-31 20:16:12 +0100

    Add functionality for dynamic window-frame load on icon click

--- js/app.js
+++ js/app.js
@@ -1,5 +1,43 @@
+// Import custom elements
+import './window-frame.js'
+
+// Display current time
 displayCurrentTime();
 
+
+// Loaded applications counter (for smooth mutual positioning)
+let windowCounter = 0;
+
+// Applications start buttons
+const memoryGameButton = document.querySelector('#memory-game-icon');
+const chatButton = document.querySelector('#chat-icon');
+const calculationGameButton = document.querySelector('#calculation-game-icon');
+
+// Buttons listen for events:
+memoryGameButton.addEventListener('click', e => {
+  const memoryGame = document.createElement('window-frame')
+  document.querySelector('.playground').appendChild(memoryGame)
+  windowCounter++
+})
+
+chatButton.addEventListener('click', e => {
+  const chat = document.createElement('window-frame')
+  document.querySelector('.playground').appendChild(chat)
+  windowCounter++
+})
+
+calculationGameButton.addEventListener('click', e => {
+  const calculationGame = document.createElement('window-frame')
+  document.querySelector('.playground').appendChild(calculationGame)
+  windowCounter++
+})
+
+// Export loaded applications counter
+export { windowCounter }
+
+
+
+// Assisting methods
 function displayCurrentTime() {
   let currentTime = new Date();
   let hours = currentTime.getHours();
