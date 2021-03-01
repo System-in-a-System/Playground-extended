fb0acd0fa7a10c8da3fbfafd9ad8a942df530800
Maryna <textconstruction@protonmail.com>
2021-03-01 16:29:05 +0100

    Add functionality for "weather-app" custom element

--- js/weather-app.js
+++ js/weather-app.js
@@ -5,42 +5,40 @@ import { WindowFrame } from "./window-frame.js";
 const template = document.createElement("template");
 
 template.innerHTML = `
-<div id="appContainer
-    style="
-      display: flex;
-      flex-direction: rows;
-      justify-content: center;
-      align-items: center">
-  
-    <div id="weatherInfo" tabindex="0" 
-      style="
-        width: fit-content; 
-        height: fit-content;
-        display: flex; 
-        flex-direction: rows;
-        justify-content: center;
-        align-items: center;
-        background-color: red;
-        border-style: solid;
-        border-width: 0.5px;
-        border-color: black;">
-    </div>
+<div id="app-container"
+  style="
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 327px;
+    background: rgb(224,224,237);
+    background: radial-gradient(circle, rgba(224,224,237,0.9867297260701156) 63%, rgba(97,115,180,1) 89%, rgba(5,3,42,1) 100%);">
+    
+      <img src="" alt="" srcset="" id="weather-icon">
+      <div id="location" style="font-size: 20px;">Unable to Fetch Weather</div>
+      <div id="description">No Information Available.</div>
+      <div id="weather-temperature"
+        style="
+          display: flex; 
+          align-items: center;
+          margin: 15px 0;
+          font-size: 14px;">
 
-  <div id="cityChoice" 
-    style="
-      margin-top: 10px;
-      width: 92%;
-      height: 30px;
-      text-align: center;
-      font-size: 17px;
-      padding: 10px;
-      background-color: rgb(214, 210, 210);
-      opacity: 0.6;">
-        
-        <div id="currentCity">Current City</div>
-        <button id="cityChoiceButton">Choose the City</button>
-
-  </div>
+            <div id="c">Error</div>
+            <div id="circle"
+              style="
+                background-color: black;
+                border-radius: 50px;
+                height: 15px;
+                width: 15px;
+                margin: 0 15px;"></div>
+            <div id="f">Error</div>
+      </div>
+      <div id="sun-info" style="font-size: 10px;">
+            <p>Sunrise: <span id="sunrise">No Information Available</span></p>
+            <p>Sunset: <span id="sunset">No Information Available</span></p>
+      </div>
 </div>
 `;
 
@@ -61,11 +59,20 @@ class WeatherApp extends WindowFrame {
     this._appTitle.textContent = "Weather";
 
     // Reference the structural parts of the appended 'weather-app' template
-    this._appContainer = this._contentBlock.querySelector("#appContainer");
-    this._weatherInfo = this._contentBlock.querySelector("#weatherInfo");
-    this._cityChoice = this._contentBlock.querySelector("#cityChoice");
-    this._currentCity = this._contentBlock.querySelector("#currentCity");
-    this._cityChoiceButton = this._contentBlock.querySelector("#cityChoiceButton");
+    this._appContainer = this._contentBlock.querySelector("#app-container");
+    this._weatherIcon = this._contentBlock.querySelector("#weather-icon");
+    this._location = this._contentBlock.querySelector("#location");
+    this._description = this._contentBlock.querySelector("#description");
+    this._weatherTemperature = this._contentBlock.querySelector("#weather-temperature");
+    this._temperatureInCelsius = this._contentBlock.querySelector("#c");
+    this._temperatureInFahrenheit = this._contentBlock.querySelector("#f");
+    this._weatherCircle = this._contentBlock.querySelector("#circle");
+    this._sunInfo = this._contentBlock.querySelector("#sun-info");
+    this._sunrise = this._contentBlock.querySelector("#sunrise");
+    this._sunset = this._contentBlock.querySelector("#sunset");
+
+    // OpenWeatherMap API.
+    this._weatherApi = "139a07dae0b4078bab359dbb8c3af430";
   }
 
   /**
@@ -74,7 +81,7 @@ class WeatherApp extends WindowFrame {
    * @memberof WeatherApp
    */
   connectedCallback() {
-    this.positionElement(); 
+    this.positionElement();
 
     this.displayWeather();
 
@@ -92,14 +99,53 @@ class WeatherApp extends WindowFrame {
   /**
    * Displays current Weather in the current City
    *
-   * @param {*} city
    * @memberof WeatherApp
    */
-  displayWeather(city) {
-    this._weatherInfo.textContent = "Weather Info";    
-  }
+  displayWeather() {  
+    // If user current Geolocation access was successful 
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        // Retrieve user Geolocation
+        const longitude = position.coords.longitude;
+        const latitude = position.coords.latitude;
 
-  
+        // Weather API base URL
+        const base = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${this._weatherApi}&units=metric`;
+
+        // Fetch Weather API
+        fetch(base)
+          .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            console.log(data);
+            const { temp } = data.main;
+            const place = data.name;
+            const { description, icon } = data.weather[0];
+            const { sunrise, sunset } = data.sys;
+
+            // Retrieve suitable icon
+            const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+
+            // Convert Celsius to Fahrenheit
+            const fahrenheit = (temp * 9) / 5 + 32;
+
+            // Convert Unix time to GMT
+            const sunriseGMT = new Date(sunrise * 1000);
+            const sunsetGMT = new Date(sunset * 1000);
+
+            // Populate DOM elements with freshly fetched weather data
+            this._weatherIcon.src = iconUrl;
+            this._location.textContent = `${place}`;
+            this._description.textContent = `${description}`[0].toUpperCase() + `${description}`.substr(1) ;
+            this._temperatureInCelsius.textContent = `${temp.toFixed(2)} °C`;
+            this._temperatureInFahrenheit.textContent = `${fahrenheit.toFixed(2)} °F`;
+            this._sunrise.textContent = `${sunriseGMT.toLocaleDateString()}, ${sunriseGMT.toLocaleTimeString()}`;
+            this._sunset.textContent = `${sunsetGMT.toLocaleDateString()}, ${sunsetGMT.toLocaleTimeString()}`;
+          });
+      });
+    }
+  }
 }
 
 // Register 'weather-app' custom element
